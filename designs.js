b93febf2cbf409e3cb14716ddc4e9f4ed990b109
Yemi Adetoye <yemi.adetoye@sciocarre.com>
2018-07-01 22:43:12 +0100

    Udacity final project for front end web beginner course
    A project to create a a dynamic table which can be used to make art using mouseover attributes

--- designs.js
+++ designs.js
@@ -3,8 +3,40 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+//var = row, column, color;
+	
+//	color = $("#colorPicker").val();
+		
+//input for the grid
 
-// Your code goes here!
-
+$(document).ready(function(){
+	
+	$("#sizePicker").submit(function(event) {
+		/*"use strict";*/
+		event.preventDefault();
+		
+		var row = $("#inputHeight").val();
+		var column = $("inputWidth").val();
+		
+		
+		//$("#pixelCanvas").empty();
+		makeGrid(row, column);
+	});
+});
+	
+//draw the grid
+function makeGrid(row, column) {
+	
+for (var i = 1; i <= row; i++) {
+		$("#pixelCanvas").append("<tr></tr>");
+	for (var j = 1; j <= column; j++) {
+			$("tr").last.append("<td></td>");
+		}
+	}
 }
+
+//add color to grid squares on mouseover
+var color = $("#colorPicker").val();
+$("#pixelCanvas").on("click", "td", function() {
+	$(this).css(background-color, color);
+});
